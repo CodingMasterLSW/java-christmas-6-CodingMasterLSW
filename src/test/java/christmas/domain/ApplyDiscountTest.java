73318975a482819ae34138bcd2b2b6593a6ae3ea
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-11-14 21:44:45 +0900

    refactor: refactor ApplyDiscountTest

--- src/test/java/christmas/domain/ApplyDiscountTest.java
+++ src/test/java/christmas/domain/ApplyDiscountTest.java
@@ -2,28 +2,20 @@ package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ApplyDiscountTest {
 
-    private List<MenuOrder> menuOrders;
+    private MenuOrders menuOrders;
 
     @DisplayName("할인 적용 후 금액 계산")
     @Test
     void 할인_적용_후_금액(){
         String input = "티본스테이크-1,크리스마스파스타-1";
         int visitDate = 1;
-        int totalPrice = 80000;
-        menuOrders = Arrays.asList(
-                new MenuOrder("티본스테이크",1),
-                new MenuOrder("크리스마스파스타",1)
-        );
-
-        ApplyDiscount applyDiscount = new ApplyDiscount(input, visitDate, totalPrice, menuOrders);
-
+        this.menuOrders = new MenuOrders(input);
+        ApplyDiscount applyDiscount = new ApplyDiscount(visitDate, menuOrders);
         assertThat(applyDiscount.getApplyDiscountPrice()).isEqualTo(74954); // 총 주문금액 80000 - 할인합계 5046
     }
 
