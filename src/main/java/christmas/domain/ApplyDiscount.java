c2a46eace7bb7b2699385f35b9e487dfaba43031
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-11-14 16:39:33 +0900

    refactor: change applyDiscount -> create object external and delete String input

--- src/main/java/christmas/domain/ApplyDiscount.java
+++ src/main/java/christmas/domain/ApplyDiscount.java
@@ -1,37 +1,26 @@
 package christmas.domain;
 
-import java.util.List;
 
 public class ApplyDiscount {
 
-    private MenuOrders menuOrders;
-    private AllDiscountCalculate allDiscountCalculate;
+    private final MenuOrders menuOrders;
+    private final AllDiscountCalculate allDiscountCalculate;
     private int applyDiscountPrice;
 
-    public ApplyDiscount(String input, int visitDate, int totalPrice, List<MenuOrder> menuOrders) {
-        initialized(input, visitDate, totalPrice, menuOrders);
-        this.applyDiscountPrice = 0;
-        addTotalPriceToAppliedDiscount();
-        subtractDiscountFromAppliedDiscount();
+    public ApplyDiscount(int visitDate, MenuOrders menuOrders) {
+        this.menuOrders = menuOrders;
+        int totalPrice = menuOrders.calculateTotalPrice();
+        this.allDiscountCalculate = new AllDiscountCalculate(visitDate, totalPrice, menuOrders.getMenuOrders());
+        this.applyDiscountPrice = calculateFinalPrice();
     }
 
-    private void initialized(String input, int visitDate, int totalPrice,
-            List<MenuOrder> menuOrders) {
-        this.menuOrders = new MenuOrders(input);
-        this.allDiscountCalculate = new AllDiscountCalculate(visitDate, totalPrice, menuOrders);
+    private int calculateFinalPrice() {
+        int totalDiscount = allDiscountCalculate.getAllDiscountPrice();
+        return menuOrders.calculateTotalPrice() - totalDiscount;
     }
 
-    private void addTotalPriceToAppliedDiscount() {
-        this.applyDiscountPrice += menuOrders.calculateTotalPrice();
-    }
-
-    private void subtractDiscountFromAppliedDiscount() {
-        this.applyDiscountPrice -= allDiscountCalculate.getAllDiscountPrice();
-    }
-
-    public int getApplyDiscountPrice(){
+    public int getApplyDiscountPrice() {
         return applyDiscountPrice;
     }
-
 }
 
